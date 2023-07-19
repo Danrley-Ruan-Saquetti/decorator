09cc12a9d226ce3663c09cedbdbd7921be242deb
Dan Ruan <101991292+Danrley-Ruan-Saquetti@users.noreply.github.com>
2023-07-19 19:29:22 +0000

    .

--- src/index.ts
+++ src/index.ts
@@ -5,43 +5,91 @@ import {
   ParameterDecorator,
 } from "@decorators/decorator";
 
-@ClassDecorator
-class teste {
-  @PropertyDecorator
-  teste;
+// @ClassDecorator
+// class teste {
+//   @PropertyDecorator
+//   teste;
 
-  constructor() {
-    this.teste = "teste";
+//   constructor() {
+//     this.teste = "teste";
+//   }
+
+//   @MethodDecorator()
+//   abc(@ParameterDecorator() param) {}
+// }
+
+// function first() {
+//   console.log("first(): factory evaluated");
+//   return function (
+//     target: any,
+//     propertyKey: string,
+//     descriptor: PropertyDescriptor
+//   ) {
+//     console.log("first(): called");
+//   };
+// }
+
+// function second() {
+//   console.log("second(): factory evaluated");
+//   return function (
+//     target: any,
+//     propertyKey: string,
+//     descriptor: PropertyDescriptor
+//   ) {
+//     console.log("second(): called");
+//   };
+// }
+
+// class ExampleClass {
+//   @first()
+//   @second()
+//   method() {}
+// }
+
+function logExecutionDate(
+  target: any,
+  propertyName: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+
+  descriptor.value = function (...args: any[]) {
+    const currentDate = new Date().toLocaleString();
+    console.log(`Method ${propertyName} executed on: ${currentDate}`);
+    return originalMethod.apply(this, args);
+  };
+
+  return descriptor;
+}
+
+// Segundo decorator para calcular o tempo de execução do método
+function logExecutionTime(
+  target: any,
+  propertyName: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+
+  descriptor.value = function (...args: any[]) {
+    console.time(`Execution time for ${propertyName}`);
+    const result = originalMethod.apply(this, args);
+    console.timeEnd(`Execution time for ${propertyName}`);
+    return result;
+  };
+
+  return descriptor;
+}
+
+class MyClass {
+  @logExecutionDate
+  @logExecutionTime
+  public someMethod() {
+    // Simulação de um método que leva algum tempo para ser executado
+    for (let i = 0; i < 1000000000; i++) {
+      // Faz alguma computação...
+    }
   }
-
-  @MethodDecorator()
-  abc(@ParameterDecorator() param) {}
 }
 
-function first() {
-  console.log("first(): factory evaluated");
-  return function (
-    target: any,
-    propertyKey: string,
-    descriptor: PropertyDescriptor
-  ) {
-    console.log("first(): called");
-  };
-}
-
-function second() {
-  console.log("second(): factory evaluated");
-  return function (
-    target: any,
-    propertyKey: string,
-    descriptor: PropertyDescriptor
-  ) {
-    console.log("second(): called");
-  };
-}
-
-class ExampleClass {
-  @first()
-  @second()
-  method() {}
-}
+const myClass = new MyClass();
+myClass.someMethod();
