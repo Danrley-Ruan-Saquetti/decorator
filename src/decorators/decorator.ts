e06f231f98dfe05505a643cf105dcb328bb8fa80
Dan Ruan <101991292+Danrley-Ruan-Saquetti@users.noreply.github.com>
2023-10-10 10:10:12 -0300

    Update decorator.ts

--- src/decorators/decorator.ts
+++ src/decorators/decorator.ts
@@ -1,23 +1,11 @@
-export function ClassDecorator(constructor) {
-  constructor.api = "Teste";
+export function ClassDecorator(constructor: any) {}
 
-  console.log(constructor);
-}
+export function PropertyDecorator(target: any, key: string) {}
 
-export function PropertyDecorator(target: any, key: string) {
-  console.log(target, key);
-}
-
-export function MethodDecorator(value: boolean = false) {
-  return (target: any, key: string, descriptor: PropertyDescriptor) => {
-    descriptor["MethodDecorator"] = value;
-  };
+export function MethodDecorator(value = false) {
+    return (target: any, key: string, descriptor: PropertyDescriptor) => {}
 }
 
 export function ParameterDecorator() {
-  return (target: any, propertyKey: string, parameterIndex: number) => {
-    console.log("target", target);
-    console.log("property key", propertyKey);
-    console.log("parameter index", parameterIndex);
-  };
+    return (target: any, propertyKey: string, parameterIndex: number) => {}
 }
