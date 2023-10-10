54ec22665b1f76fe29a9f1e49aa70e50231457b6
Dan Ruan <101991292+Danrley-Ruan-Saquetti@users.noreply.github.com>
2023-10-10 10:20:50 -0300

    Delete src/decorators directory

--- src/decorators/decorator.ts
+++
@@ -1,7 +0,0 @@
-export function ClassDecorator(constructor: any) {}
-
-export function AttributeDecorator(target: any, key: string) {}
-
-export function MethodDecorator(target: any, key: string, descriptor: PropertyDescriptor) {}
-
-export function ParameterDecorator(target: any, propertyKey: string, parameterIndex: number) {}
